83f9fefa05015ca6b46533328910f73f7e4a0ee0
rokpopov <rokpopov90@gmail.com>
2021-12-05 17:49:38 +0100

    create getPosts function to fetch data from db, join posts and users db tables, create aliases for posts and users DB table ID fields, use getPosts() in Posts controller - pass the value into $data array so it can be accessible in the posts view

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -9,4 +9,12 @@
       </a>
     </div>
   </div>
+  <?php foreach($data['posts'] as $post) : ?>
+    <div class="card card-body mb-3 bg-dark">
+      <h4 class="card-title text-light"><?php echo $post->title; ?></h4>
+      <p class="card-text text-secondary">Written by  <?php echo $post->name; ?> on <?php echo $post->postCreated; ?></p>
+      <p class="card-text text-light"><?php echo $post->body; ?></p>
+      <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-secondary">More</a>
+    </div>
+  <?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
